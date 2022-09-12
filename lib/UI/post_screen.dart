2d31a12bc2050268a0b842a6c8369850859fa9d4
kamran saleem <freinds221@gmail.com>
2022-09-12 15:42:02 +0500

    firesstore

--- lib/UI/post_screen.dart
+++ lib/UI/post_screen.dart
@@ -29,6 +29,8 @@ class _PostScreenState extends State<PostScreen> {
 
   final fireStoreData =
       FirebaseFirestore.instance.collection('posts').snapshots();
+  CollectionReference reference =
+      FirebaseFirestore.instance.collection('posts');
 
   @override
   void dispose() {
@@ -190,9 +192,53 @@ class _PostScreenState extends State<PostScreen> {
                         return ListView.builder(
                             itemCount: snapshot.data!.docs.length,
                             itemBuilder: ((context, index) {
+                              final String title = snapshot
+                                  .data!.docs[index]['title']
+                                  .toString();
                               return ListTile(
                                 title: Text(snapshot.data!.docs[index]['title']
                                     .toString()),
+                                trailing: PopupMenuButton(
+                                    icon: const Icon(Icons.more_vert),
+                                    itemBuilder: ((context) => [
+                                          PopupMenuItem(
+                                              child: ListTile(
+                                            onTap: (() {
+                                              Navigator.pop(context);
+                                              // Here we have tp pass title and id of that title to out dunction
+                                              // We got the ID by firebaseAnimated list
+                                              showMyDialogue(
+                                                  title,
+                                                  snapshot.data!.docs[index].id
+                                                      .toString());
+                                            }),
+                                            title: const Text('Edit'),
+                                            leading: const Icon(Icons.edit),
+                                          )),
+                                          PopupMenuItem(
+                                              child: ListTile(
+                                            onTap: (() {
+                                              Navigator.pop(context);
+
+                                              // Here we used the ID to delete message from databse
+
+                                              reference
+                                                  .doc(snapshot
+                                                      .data!.docs[index].id
+                                                      .toString())
+                                                  .delete()
+                                                  .then((value) {
+                                                Utils.toatsMessage(
+                                                    'Deleted Succefully');
+                                              }).onError((error, stackTrace) {
+                                                Utils.toatsMessage(
+                                                    error.toString());
+                                              });
+                                            }),
+                                            title: const Text('delete'),
+                                            leading: const Icon(Icons.delete),
+                                          ))
+                                        ])),
                               );
                             }));
                       }
@@ -233,12 +279,22 @@ class _PostScreenState extends State<PostScreen> {
                     // To update data we are gonna use the ID of that message
                     // For that we would use instance of the database we created
                     // And would give id as a path to it
+
                     refer
                         .child(id)
                         .update({'title': edit.text.toString()}).then((value) {
                       Utils.toatsMessage('Updated');
                     }).onError((error, stackTrace) {
                       Utils.flushBarSnakError(error.toString(), context);
+                    });
+
+                    // This is for firestore databse
+                    reference.doc(id).update({
+                      'title': edit.text.toString(),
+                    }).then((value) {
+                      Utils.toatsMessage("Updated");
+                    }).onError((error, stackTrace) {
+                      Utils.toatsMessage(error.toString());
                     });
                   },
                   child: const Text('Update')),
